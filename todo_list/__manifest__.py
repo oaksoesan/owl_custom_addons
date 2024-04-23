ab6174a1ee21f50d0fd3d40ac97978906652c465
Oak Soe San <pm@mingalarsky.com>
2024-04-23 21:58:44 +0630

    Initialization ============== 1. Created a basic Odoo App Structure

---
+++ todo_list/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'To Do List',
+    'description': '''
+         A simple To-Do list using Odoo's OWL framework
+    ''',
+    'version': '1.0',
+    'sequence': -1,
+    'category': 'OWL',
+    'depends': [
+        'base',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets.backend': [
+
+        ],
+        'web.assets.frontend': [
+
+        ],
+    }
+
+}
